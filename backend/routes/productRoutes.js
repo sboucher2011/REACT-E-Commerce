481dbc8df32b7a3743c8b305bd00fe6743b2928d
sboucher <sboucher2011@gmail.com>
2022-07-04 11:41:35 -0400

    added user backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,11 @@
-import express from 'express'
-import asyncHandler from 'express-async-handler'
-const router = express.Router()
-import Product from '../models/productModel.js'
+import express from "express";
+const router = express.Router();
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
-// Get All Products
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-    res.json(products)
-  })
-)
-
-// Get single Product
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-  })
-)
-
-export default router
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.use(express.json());
 
-app.use('/api/products', productRoutes)
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.use(notFound)
-app.use(errorHandler)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
